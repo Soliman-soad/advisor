0506a698e4d908376826a77acf1667442a86e5f7
Soliman-soad <mdsolimansoad@gmail.com>
2023-02-24 14:20:17 +0600

    slider section done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { Nav } from './components/Shared/Nav';
+import { Footer } from './components/Shared/Footer';
+import SliderBanner from './components/SliderBanner';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <SliderBanner/>
+      <Footer/>
     </div>
   );
 }
